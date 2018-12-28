b74e31280124bd925ba7f95c468fb7931a0165ae
KimMooHyeon <dlfb77@gmail.com>
2018-12-28 19:31:47 +0900

    editText->TextView change

--- app/src/main/java/com/computer/inu/myworkinggings/SignUp2Activity.kt
+++ app/src/main/java/com/computer/inu/myworkinggings/SignUp2Activity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import kotlinx.android.synthetic.main.activity_sign_up2.*
-import kotlinx.android.synthetic.main.activity_test.*
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 

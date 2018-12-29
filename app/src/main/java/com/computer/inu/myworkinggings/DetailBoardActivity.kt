6e56be6cbdef09b01bad5d55dddb6db41af1be3c
KimMooHyeon <dlfb77@gmail.com>
2018-12-29 14:29:58 +0900

    add detailboard activity

---
+++ app/src/main/java/com/computer/inu/myworkinggings/DetailBoardActivity.kt
@@ -0,0 +1,12 @@
+package com.computer.inu.myworkinggings
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailBoardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_board)
+    }
+}

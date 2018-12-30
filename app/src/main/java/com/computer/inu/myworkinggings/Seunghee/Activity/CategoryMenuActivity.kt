c903a0f7aa710dc4170a0b417ba59f0379dc9e76
seunghee63 <0603yang@naver.com>
2018-12-30 20:10:25 +0900

    add homeboard category menu bar

---
+++ app/src/main/java/com/computer/inu/myworkinggings/Seunghee/Activity/CategoryMenuActivity.kt
@@ -0,0 +1,13 @@
+package com.computer.inu.myworkinggings.Seunghee.Activity
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import com.computer.inu.myworkinggings.R
+
+class CategoryMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_category_menu)
+    }
+}

1fb44a839cf6788d15fdc49488a4a8abb26caa24
KimMooHyeon <dlfb77@gmail.com>
2019-01-01 20:49:50 +0900

    modify sign_up and etc

---
+++ app/src/main/java/com/computer/inu/myworkinggings/Moohyeon/Activity/SignUp3Activity.kt
@@ -0,0 +1,18 @@
+package com.computer.inu.myworkinggings.Moohyeon.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.computer.inu.myworkinggings.R
+import kotlinx.android.synthetic.main.activity_sign_up3.*
+import org.jetbrains.anko.startActivity
+
+class SignUp3Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up3)
+        tv_sign_up3_sign_up_complete.setOnClickListener {
+            startActivity<LoginActivity>()
+        }
+    }
+}
--- app/src/main/java/com/computer/inu/myworkinggings/Moohyeon/Adapter/MyFragmentStatePagerAdapter.kt
+++ app/src/main/java/com/computer/inu/myworkinggings/Moohyeon/Adapter/MyFragmentStatePagerAdapter.kt
@@ -1,14 +1,24 @@
 package com.computer.inu.myworkinggings.adapter
 
+import android.content.Context
+import android.content.Context.CONTEXT_RESTRICTED
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
+import android.support.v4.view.ViewPager
 import com.computer.inu.myworkinggings.Hyunjin.Activity.TopNaviMessageNoticeActivity
 import com.computer.inu.myworkinggings.Hyunjin.Fragment.RoungeFragment
 import com.computer.inu.myworkinggings.Jemin.Fragment.MyPageFragment
+import com.computer.inu.myworkinggings.Moohyeon.Fragment.DirectoryFragment
 import com.computer.inu.myworkinggings.Seunghee.Fragment.HomeBoardFragment
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.support.v4.content.ContextCompat.getSystemService
+import android.view.inputmethod.InputMethod
+import android.view.inputmethod.InputMethodManager
+import android.support.v4.content.ContextCompat.getSystemService
 
-import com.computer.inu.myworkinggings.fragment.*
+
+
 
 class MyFragmentStatePagerAdapter(fm : FragmentManager, val fragmentCount : Int): FragmentStatePagerAdapter(fm) {
     override fun getItem(position: Int): Fragment? {

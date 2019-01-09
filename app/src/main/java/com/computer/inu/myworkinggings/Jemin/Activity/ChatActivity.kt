61d8e2a438a704d9abe61b7ee6bc89535d72e34c
SongJemin <jemin56@naver.com>
2019-01-10 01:26:23 +0900

    ChatActivity Resize and EditText setText("") after send message

--- app/src/main/java/com/computer/inu/myworkinggings/Jemin/Activity/ChatActivity.kt
+++ app/src/main/java/com/computer/inu/myworkinggings/Jemin/Activity/ChatActivity.kt
@@ -1,9 +1,10 @@
 package com.computer.inu.myworkinggings.Jemin.Activity
 
+import android.os.*
 import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.computer.inu.myworkinggings.Hyunjin.Adapter.MessageSendDataRecyclerViewAdapter
 import com.computer.inu.myworkinggings.Hyunjin.Data.MessageSendData
@@ -35,9 +36,16 @@ class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+        sendMessage = "메세지 보냅니다."
+        chat_send_btn.setOnClickListener {
+            sendMessage = chat_message_edit.text.toString()
+            chat_message_edit.setText("")
+            sendMessage()
+        }
 
         var headers = java.util.ArrayList<StompHeader>()
         headers.add(StompHeader("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjksInJvbGUiOiJVU0VSIiwiaXNzIjoiR2luZ3MgVXNlciBBdXRoIE1hbmFnZXIiLCJleHAiOjE1NDkxOTYxMzN9.OrlfMuYaMa2SqrXGcHlDRmttGOC1z7DiROKD4dsz2Ds"))
+
 
         //서버연결
         mStompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, "ws://52.78.243.92:8080/connect")
@@ -56,10 +64,68 @@ class ChatActivity : AppCompatActivity() {
             receiveMessage = jObject.getString("message")
             Log.v(TAG, "Receive Message = " + receiveMessage)
 
+
+                if(receiveMessage.isNotEmpty()){
+                    Log.v(TAG,"수신 메세지 not null2")
+                    Log.v(TAG,"채팅 데이터 크기 = " + chatData.size)
+                    chatData.add(ChatListItem(1,"받는 메세지 = " + receiveMessage))
+
+                    chatAdapter = ChatAdapter(chatData)
+                    setAdapter()
+
+                }
+
         }
 
+        // edittext 클릭 시 채팅리스트 맨 아래로 가도록
+        if (Build.VERSION.SDK_INT >= 11) {
+            chat_chat_recyclerview.addOnLayoutChangeListener(View.OnLayoutChangeListener { v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
+                if (bottom < oldBottom) {
+                    chat_chat_recyclerview.postDelayed(Runnable {
+                        // 대화 내용이 없는 경우
+                        if(chat_chat_recyclerview.getAdapter()==null){
+                        }
+                        // 대화 내용이 있는 경우
+                        else{
+                            chat_chat_recyclerview.smoothScrollToPosition(
+                                    chat_chat_recyclerview.getAdapter()!!.getItemCount() - 1)
+                        }
+                    }, 100)
+                }
+            })
+        }
+
+        sendMessage()
+/*
+        while(receiveMessage.isEmpty()){
+            Log.v(TAG,"수신 메세지 null")
+            if(receiveMessage.isNotEmpty()){
+                Log.v(TAG,"수신 메세지 not null2")
+                Log.v(TAG,"채팅 데이터 크기 = " + chatData.size)
+                chatData.add(ChatListItem(1,"받는 메세지 = " + receiveMessage))
+                chatAdapter = ChatAdapter(chatData)
+                chat_chat_recyclerview.adapter = chatAdapter
+                chat_chat_recyclerview.layoutManager = LinearLayoutManager(this)
+            }
+        }
+        */
+        Log.v(TAG,"수신 메세지 not null")
+
+
+    }
+
+
+
+    protected fun applySchedulers(): CompletableTransformer {
+        return CompletableTransformer { upstream ->  upstream.unsubscribeOn(Schedulers.newThread()).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())}
+    }
+
+    fun setRecyclerView() {
+
+
+    }
+    fun sendMessage(){
         //메세지 보내기
-        sendMessage = "메세지 보냅니다."
         Log.v(TAG, "Send Message = " + sendMessage)
         chatData.add(ChatListItem(0,"보낸 메세지 = " + sendMessage))
         var mTimeFormat = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
@@ -72,30 +138,41 @@ class ChatActivity : AppCompatActivity() {
                     Log.e("SendLogError", "Error send STOMP echo", throwable)
                     Toast.makeText(applicationContext, throwable.message, Toast.LENGTH_SHORT).show()
                 })
+        chatAdapter = ChatAdapter(chatData)
+        chat_chat_recyclerview.adapter = chatAdapter
+        chat_chat_recyclerview.layoutManager = LinearLayoutManager(this)
+        chat_chat_recyclerview.scrollToPosition(chatData.size-1)
+    }
 
-        while(receiveMessage.isEmpty()){
-            Log.v(TAG,"수신 메세지 null")
-            if(receiveMessage.isNotEmpty()){
-                Log.v(TAG,"수신 메세지 not null2")
-                Log.v(TAG,"채팅 데이터 크기 = " + chatData.size)
-                chatData.add(ChatListItem(1,"받는 메세지 = " + receiveMessage))
-                chatAdapter = ChatAdapter(chatData)
-                chat_chat_recyclerview.adapter = chatAdapter
-                chat_chat_recyclerview.layoutManager = LinearLayoutManager(this)
+    fun setAdapter(){
+        object : Thread() {
+
+            override fun run() {
+
+                Log.v(TAG,"스레드 런")
+                val msg = handler.obtainMessage()
+
+                handler.sendMessage(msg)
+
+
             }
+
+        }.start()
+
+    }
+
+    internal val handler: Handler = object : Handler(Looper.getMainLooper()) {
+
+        override fun handleMessage(msg: Message) {
+            Log.v(TAG,"핸들러 런")
+            chat_chat_recyclerview.adapter = chatAdapter
+            chat_chat_recyclerview.layoutManager = LinearLayoutManager(applicationContext)
+            chat_chat_recyclerview.scrollToPosition(chatData.size-1)
+            // 원래 하고싶었던 일들 (UI변경작업 등...)
+
         }
-        Log.v(TAG,"수신 메세지 not null")
-
-
 
     }
 
-    protected fun applySchedulers(): CompletableTransformer {
-        return CompletableTransformer { upstream ->  upstream.unsubscribeOn(Schedulers.newThread()).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())}
-    }
 
-    fun setRecyclerView() {
-
-
-    }
 }
--- app/src/main/java/com/computer/inu/myworkinggings/Jemin/Activity/Test2Activity.java
+++ app/src/main/java/com/computer/inu/myworkinggings/Jemin/Activity/Test2Activity.java
@@ -1,6 +1,9 @@
 package com.computer.inu.myworkinggings.Jemin.Activity;
 
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -14,11 +17,28 @@ import gun0912.tedbottompicker.TedBottomPicker;
 
 public class Test2Activity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test2);
+        Handler handler = new Handler();
+        new Thread()
 
+        {
+
+            public void run()
+
+            {
+
+                Message msg = handler.obtainMessage();
+
+                handler.sendMessage(msg);
+
+            }
+
+        }.start();
         TedBottomPicker tedBottomPicker = new TedBottomPicker.Builder(Test2Activity.this)
                 .setOnMultiImageSelectedListener(new TedBottomPicker.OnMultiImageSelectedListener() {
                     @Override
@@ -33,6 +53,22 @@ public class Test2Activity extends AppCompatActivity {
                 .create();
 
     }
+
+    final Handler handler = new Handler(Looper.getMainLooper())
+
+    {
+
+        public void handleMessage(Message msg)
+
+        {
+
+            // 원래 하고싶었던 일들 (UI변경작업 등...)
+
+        }
+
+    };
+
+
 
 
 

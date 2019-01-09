4331a534e033936c254eb4a3b6aab6492ba22f7e
SongJemin <jemin56@naver.com>
2019-01-10 07:07:30 +0900

    Merge branch 'song' of https://github.com/seunghee63/Gings-Android into jemin

--- app/src/main/java/com/computer/inu/myworkinggings/Seunghee/Adapter/BoardRecyclerViewAdapter.kt
+++ app/src/main/java/com/computer/inu/myworkinggings/Seunghee/Adapter/BoardRecyclerViewAdapter.kt
@@ -136,7 +136,7 @@ class BoardRecyclerViewAdapter(val ctx: Context, var dataList: ArrayList<BoardIt
             ctx.toast(dataList[position].boardId!!.toString())
 
             //본인 게시글 클릭
-            ctx.startActivity<HomeBoardMoreBtnMineActivity>("BoardId" to dataList[position].boardId, "ㅎ" to this)
+            ctx.startActivity<HomeBoardMoreBtnMineActivity>("BoardId" to dataList[position].boardId)
 
             //일반 게시글 클릭
             ctx.startActivity<HomeBoardMoreBtnActivity>("BoardId" to dataList[position].boardId)
